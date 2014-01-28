40a28ecc7667ad2fefbc163af7d8e0d7a3ce4561
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-29 01:47:39 +0900

    elasticsearchサーバーへアクセスしてデータを取って表示までできた

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,3 +1,9 @@
 $(function() {
-    $("#s").val("test");
+    $.ajax("http://192.168.33.10:9200/overlay_tips/tip/1").done(
+        function(data, textStatus, jqXHR) {
+            console.log(data);
+            $("#s").val(data._source.tip);
+        }
+    );
+    //$("#s").val("test");
 });
