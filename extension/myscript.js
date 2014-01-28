5e9ab787e116ca9294edeb6d8f8b1ff6ef66808e
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-29 02:34:31 +0900

    ついにセレクターで表示できるようになった。BANZAI

--- extension/myscript.js
+++ extension/myscript.js
@@ -2,8 +2,7 @@ $(function() {
     $.ajax("http://192.168.33.10:9200/overlay_tips/tip/1").done(
         function(data, textStatus, jqXHR) {
             console.log(data);
-            $("#s").val(data._source.tip);
+            $(data._source.selector).append("<br /> " + data._source.tip);
         }
     );
-    //$("#s").val("test");
 });
