fb05d3c6b36509840331432816e87f2e9f263687
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-29 00:59:42 +0900

    とりあえずelasticsearchのページの検索ボックスへ文字を入れることに成功した

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,25 +1,3 @@
-function getShortenedUrl(url) {
-	var hostPattern = new RegExp("^(.*?://.*?/)");
-	var queryPattern = new RegExp("[\\?&](q=.*?)[&$#]");
-	var path = url.match(hostPattern);
-	var query = url.match(queryPattern);
-	if (path && query)
-		return path[1] + "search?" + query[1];
-	else
-		return null;
-}
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (getShortenedUrl(tab.url)) {
-		chrome.pageAction.show(tabId);
-	} else {
-		chrome.pageAction.hide(tabId);
-	}
-});
-
-chrome.pageAction.onClicked.addListener(function(tab) {
-	var shortenedUrl = getShortenedUrl(tab.url);
-	if(shortenedUrl) {
-		chrome.tabs.update(tab.id, {url: shortenedUrl});
-	}
+$(function() {
+    $("#s").val("test");
 });
