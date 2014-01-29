3362ef6f7874fb5eb952daa2b783a4c00d4b79d0
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-30 02:38:24 +0900

    フラグによってデータを表示するかどうかを振り分けたいが意図通り動かない
     * プラグインをリロードしないと表示/非表示が切り替わらない。なぜ？

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,8 +1,10 @@
 $(function() {
-    $.ajax("http://192.168.33.10:9200/overlay_tips/tip/1").done(
-        function(data, textStatus, jqXHR) {
-            console.log(data);
-            $(data._source.selector).append("<br /> " + data._source.tip);
-        }
-    );
+    if (localStorage["extensionIsEnable"] == "true") {
+        $.ajax("http://192.168.33.10:9200/overlay_tips/tip/1").done(
+            function(data, textStatus, jqXHR) {
+                console.log(data);
+                $(data._source.selector).append("<br /> " + data._source.tip);
+            }
+        );
+    }
 });
