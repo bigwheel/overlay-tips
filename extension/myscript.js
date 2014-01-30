0a9c3b8b8a9a7a5546f5f0e9a05e40c79bc0869e
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-31 03:13:34 +0900

    だいぶそれらしくなってきた

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,9 +1,16 @@
 $(function() {
     chrome.extension.sendRequest({}, function(extensionIsEnable) {
         if (extensionIsEnable === "true") {
-            $.ajax("http://192.168.33.10:9200/overlay_tips/tip/1").done(
-                function(data, textStatus, jqXHR) {
-                    $(data._source.selector).append("<br /> " + data._source.tip);
+            $.post(
+                "http://192.168.33.10:9200/overlay_tips/tip/_search",
+                JSON.stringify({ query: { term: { url: location.href } } })
+            ).done(
+                function(result, textStatus, jqXHR) {
+                    var hits = result.hits.hits;
+                    if (hits.length != 0) {
+                        var data = hits[0]._source;
+                        $(data.selector).append("<br /> " + data.tip);
+                    }
                 }
             )
         }
