8922efa7361bf689aa3b24abe67e734f43c01a86
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-31 03:37:47 +0900

    javascriptをjquery式に書きなおした

--- extension/options.js
+++ extension/options.js
@@ -1,24 +1,13 @@
-// Saves options to localStorage.
 function save_options() {
-    var select = document.getElementById("color");
-    var color = select.children[select.selectedIndex].value;
-    localStorage["favorite_color"] = color;
+    localStorage["favorite_color"] = $("#color").val();
 }
 
-// Restores select box state to saved value from localStorage.
 function restore_options() {
     var favorite = localStorage["favorite_color"];
     if (!favorite) {
         return;
     }
-    var select = document.getElementById("color");
-    for (var i = 0; i < select.children.length; i++) {
-        var child = select.children[i];
-        if (child.value == favorite) {
-            child.selected = "true";
-            break;
-        }
-    }
+    $("#color").val(favorite);
 }
 
 $(function() {
