a20f00b50e2531897bf84806cd76b4ad90c711cf
bigwheel <k.bigwheel+eng@gmail.com>
2014-01-31 09:50:44 +0900

    とりあえずpopupを作り途中

---
+++ extension/popup.js
@@ -0,0 +1,15 @@
+$(function() {
+    $.post(
+        localStorage["elasticsearch_url"] + "_search",
+        JSON.stringify({ query: { term: { url: location.href } } })
+    ).done(
+    function(result, textStatus, jqXHR) {
+         result.hits.hits;
+        if (hits.length != 0) {
+            var data = hits[0]._source;
+            $(data.selector).append("<br /> " + data.tip);
+        }
+    }
+    );
+});
+
