693d990e3a161d282e9c1ecf0e83307a0be1beb9
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-06 09:49:38 +0900

    checkboxをとりあえず作った

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,21 @@
 $(function() {
-    $.post(
-        localStorage["elasticsearch_url"] + "_search",
-        JSON.stringify({ query: { term: { url: location.href } } })
-    ).done(
-    function(result, textStatus, jqXHR) {
-         result.hits.hits;
-        if (hits.length != 0) {
-            var data = hits[0]._source;
-            $(data.selector).append("<br /> " + data.tip);
-        }
-    }
-    );
+     if (localStorage["extensionIsEnable"] === "true") {
+         $("input[type='checkbox']").attr("checked", true);
+     } else {
+         $("input[type='checkbox']").attr("checked", false);
+     }
+
+//    $.post(
+//        localStorage["elasticsearch_url"] + "_search",
+//        JSON.stringify({ query: { term: { url: location.href } } })
+//    ).done(
+//    function(result, textStatus, jqXHR) {
+//         result.hits.hits;
+//        if (hits.length != 0) {
+//            var data = hits[0]._source;
+//            $(data.selector).append("<br /> " + data.tip);
+//        }
+//    }
+//    );
 });
 
