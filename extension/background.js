07b798f5f1c50aa3fa6c250ffef5b140360de50e
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-09 20:33:02 +0900

    初期化周りを整理してどんどんlocalStorageをカプセル化した

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,7 @@
 // localStorageへ格納されている設定の初期化
-if (store.get('extensionIsEnable') === undefined) {
-    store.set('extensionIsEnable', true);
+if (extensionState.isEnable() === undefined) {
+    extensionState.isEnable(true);
 }
-
-updateBrowserActionIcon();
 
 chrome.extension.onRequest.addListener(function(_, _, sendResponse) {
     sendResponse(store.getAll());
--- extension/share.js
+++ extension/share.js
@@ -1,15 +1,14 @@
-function updateBrowserActionIcon() {
-    var fileName = store.get('extensionIsEnable') === true ?
-        'icons/icon48.png' : 'icons/icon48_disable.png';
-    var iconUrl = chrome.extension.getURL(fileName);
-    chrome.browserAction.setIcon({ 'path': iconUrl });
-}
-
 extensionState = {
-    isEnable: ko.observable(store.get('extensionIsEnable'))
+    isEnable: ko.observable(store.get('extensionIsEnable')),
+    updateBrowserActionIcon: function() {
+        var fileName = store.get('extensionIsEnable') === true ?
+            'icons/icon48.png' : 'icons/icon48_disable.png';
+        var iconUrl = chrome.extension.getURL(fileName);
+        chrome.browserAction.setIcon({ 'path': iconUrl });
+    }
 };
 extensionState.isEnable.subscribe(function(newValue) {
     store.set('extensionIsEnable', newValue);
-    updateBrowserActionIcon();
-});
+    this.updateBrowserActionIcon();
+}, extensionState);
 
