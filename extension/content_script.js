cf769401a1d9585ac9bdb0758db8a65f8b172a9c
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-09 17:22:02 +0900

    変数の無意味な短縮をやめた

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,8 +1,8 @@
 $(function() {
-    chrome.extension.sendRequest({}, function(ls) {
-        if (ls.extensionIsEnable === true) {
+    chrome.extension.sendRequest({}, function(localStorage) {
+        if (localStorage.extensionIsEnable === true) {
             $.post(
-                ls.elasticsearch_url + '_search',
+                localStorage.elasticsearch_url + '_search',
                 JSON.stringify({ query: { term: { url: location.href } } })
             ).done(
                 function(result, textStatus, jqXHR) {
