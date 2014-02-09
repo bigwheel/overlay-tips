36af16aa22e34f5fccdd0fb563abc4f7bffb3254
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-09 23:06:41 +0900

    だいぶ進んできたが、またproxy/bind関連で引っかかった。面倒だなあ

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,5 +1,19 @@
 function PageProperTipsViewModel() {
     this.result = ko.observable([]);
+    chrome.tabs.getSelected(function(tab) {
+        queryToElasticSearchByUrl(store.get('elasticSearchUrl'), tab.url).done(
+            function(result) {
+                $.each(result.hits.hits, function(_, hit) {
+                    var data = hit._source;
+                    //this.result(
+                    //    this.result().push({ tip: data.tip })
+                    //);
+                    console.log(data);
+                    //$(data.selector).append('<br /> ' + data.tip);
+                });
+            }
+        );
+    });
 }
 
 $(function() {
