6712e754363ca028efdfe436710c28e78191d2e5
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-09 21:15:09 +0900

    カプセル化をどんどん推し進めた

--- extension/share.js
+++ extension/share.js
@@ -1,14 +1,17 @@
-extensionState = {
-    isEnable: ko.observable(store.get('extensionIsEnable')),
-    updateBrowserActionIcon: function() {
+function extensionStateViewModel() {
+    this.isEnable = ko.observable(store.get('extensionIsEnable'));
+    this.updateBrowserActionIcon = $.proxy(function() {
         var filePath = (this.isEnable() ?
             'icons/icon48.png' : 'icons/icon48_disable.png');
         var iconUrl = chrome.extension.getURL(filePath);
         chrome.browserAction.setIcon({ 'path': iconUrl });
-    }
-};
-extensionState.isEnable.subscribe(function(newValue) {
-    store.set('extensionIsEnable', newValue);
-    this.updateBrowserActionIcon();
-}, extensionState);
+    });
+
+    this.isEnable.subscribe(function(newValue) {
+        store.set('extensionIsEnable', newValue);
+        this.updateBrowserActionIcon();
+    }, this);
+}
+
+extensionState = new extensionStateViewModel();
 
