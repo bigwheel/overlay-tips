6343b45b87f3ebb76414d410cf461d7b9c267a8f
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-10 02:05:49 +0900

    更新もできるようになった

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,4 +1,5 @@
 function PageProperTipsViewModel() {
+    var self = this;
     this.result = ko.observable([]);
     chrome.tabs.getSelected($.proxy(function(tab) {
         queryToElasticSearchByUrl(store.get('elasticSearchUrl'), tab.url).done(
@@ -6,14 +7,34 @@ function PageProperTipsViewModel() {
                 if (result.exists) {
                     $.each(result._source.tips, $.proxy(function(_, tip) {
                         var result = this.result();
-                        result.push({ tip: tip.tip, selector: tip.selector });
+                        result.push(tip);
                         this.result(result);
                     }, this));
                 }
             }, this)
         );
     }, this));
-    this.submit = function() { return false; };
+    this.putResult = ko.observable('');
+    this.submit = function() {
+        console.log(JSON.stringify(self.result()));
+        self.putResult('');
+        chrome.tabs.getSelected($.proxy(function(tab) {
+            $.ajax(store.get('elasticSearchUrl') + encodeURIComponent(tab.url),
+                   {
+                       type: 'PUT',
+                       contentType: 'application/json',
+                       data: JSON.stringify({ tips: self.result() }),
+                       dataType: 'json'
+                   }).
+                       done(function() {
+                       self.putResult('成功');
+                   }).
+                       fail(function() {
+                       self.putResult('失敗');
+                   });
+        }));
+        return false;
+    };
 }
 
 $(function() {
