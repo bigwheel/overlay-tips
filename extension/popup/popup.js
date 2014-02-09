5d081c97c07e84f44122a683f0ea4566ef6460b1
bigwheel <k.bigwheel+eng@gmail.com>
2014-02-09 21:52:05 +0900

    不要になったコメントアウトコードを削除した

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -5,18 +5,5 @@ function ArraySampleViewModel() {
 $(function() {
     ko.applyBindings(new extensionStateViewModel(), $('form')[0]);
     ko.applyBindings(new ArraySampleViewModel(), $('table')[0]);
-//
-//    $.post(
-//        store.get('elasticsearch_url') + '_search',
-//        JSON.stringify({ query: { term: { url: location.href } } })
-//    ).done(
-//    function(result, textStatus, jqXHR) {
-//         result.hits.hits;
-//        if (hits.length != 0) {
-//            var data = hits[0]._source;
-//            $(data.selector).append('<br /> ' + data.tip);
-//        }
-//    }
-//    );
 });
 
